e0390f3b707e723c246d57bd523c3a11c065b133
Vishal Rathod <rvd31305@gmail.com>
2022-03-11 18:15:07 +0530

    column div moved to parent component

--- src/components/ContactTable/ContactTable.js
+++ src/components/ContactTable/ContactTable.js
@@ -10,33 +10,31 @@ const ContactTable = ({
   showActiveUser,
 }) => {
   return (
-    <div className='col-md-6 '>
-      <div className='main-content-list'>
-        <Table>
-          <thead>
-            <tr>
-              <th className='text-center'>+</th>
-              <th className='text-secondary'>Basic Info</th>
-              <th className='text-secondary'>Company</th>
-              <th className='text-center'></th>
-            </tr>
-          </thead>
-          <tbody>
-            {contacts
-              .filter((contact) =>
-                contact.fname.toLowerCase().includes(filterText.toLowerCase())
-              )
-              .map((contact) => (
-                <ContactRow
-                  key={contact.id}
-                  contact={contact}
-                  deleteContact={deleteContact}
-                  showActiveUser={showActiveUser}
-                />
-              ))}
-          </tbody>
-        </Table>
-      </div>
+    <div className='main-content-list'>
+      <Table>
+        <thead>
+          <tr>
+            <th className='text-center'>+</th>
+            <th className='text-secondary'>Basic Info</th>
+            <th className='text-secondary'>Company</th>
+            <th className='text-center'></th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts
+            .filter((contact) =>
+              contact.fname.toLowerCase().includes(filterText.toLowerCase())
+            )
+            .map((contact) => (
+              <ContactRow
+                key={contact.id}
+                contact={contact}
+                deleteContact={deleteContact}
+                showActiveUser={showActiveUser}
+              />
+            ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
