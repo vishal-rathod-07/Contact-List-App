9c090a867b51dff4d912038dff175e5f7ba06b1d
Vishal Rathod <rvd31305@gmail.com>
2022-03-11 18:06:19 +0530

    Contact button component added

---
+++ src/components/ContactButton/ContactButton.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+// import ContactModel from "../ContactModel/ContactModel";
+import { PencilIcon } from "@heroicons/react/outline";
+import { PlusIcon } from "@heroicons/react/outline";
+
+import "./contactbutton.scss";
+
+const ContactButton = ({
+  btnIcon,
+  btnText,
+  setIsEdit,
+  isEdit,
+  btnType,
+  setModalShow,
+}) => {
+  //   const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button
+        variant='default'
+        onClick={() => {
+          setModalShow(true);
+          btnType === "add" ? setIsEdit(false) : setIsEdit(true);
+        }}
+        // onClick={setIsEdit(isEdit)}
+        className='contact-btn'
+      >
+        {btnIcon === "pencil" ? (
+          <PencilIcon className='edit-icon align-middle' />
+        ) : btnIcon === "plus" ? (
+          <PlusIcon className='edit-icon align-middle' />
+        ) : null}
+        {btnText}
+      </Button>
+
+      {/* <ContactModel
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        title={isEdit ? "Edit Contact" : "Add Contact"}
+      /> */}
+    </>
+  );
+};
+
+export default ContactButton;
