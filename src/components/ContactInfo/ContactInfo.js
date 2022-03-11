0945d9c10ae3faa708cb461a8e86686c513421df
Vishal Rathod <rvd31305@gmail.com>
2022-03-11 18:07:00 +0530

    Edit Button added

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -1,80 +1,116 @@
 import ContactAvatar from "../ContactAvatar/ContactAvatar";
-import EditContact from "../EditContact/EditContact";
+import ContactButton from "../ContactButton/ContactButton";
+import { useEffect, useState } from "react";
 
 import "./contactinfo.scss";
 
 const ContactInfo = (props) => {
-  const { fname, email, phone, company, Role, address } = props.activeContact;
+  const { id, fname, email, phone, company, Role, address } =
+    props.activeContact;
+
+  const [contact, setContact] = useState({
+    id: id,
+    fname: fname,
+    email: email,
+    phone: phone,
+    company: company,
+    Role: Role,
+    address: address,
+  });
+
+  useEffect(() => {
+    setContact({
+      id: id,
+      fname: fname,
+      email: email,
+      phone: phone,
+      company: company,
+      Role: Role,
+      address: address,
+    });
+  }, [id, fname, email, phone, company, Role, address]);
+
   return (
-    <div className='col-md-6'>
-      <div className='main-content-card'>
-        <div
-          className='container'
-          style={{
-            width: "90%",
-            margin: "auto",
-          }}
-        >
-          <div className='row'>
-            <div className='col-md-12'>
-              <div className='card mb-3'>
-                <div className='card-body'>
-                  <div className='d-flex flex-column align-items-center text-center'>
-                    <ContactAvatar name={fname} className={"info-avatar"} />
-                    <div className='mt-3'>
-                      <h4>{fname}</h4>
-                      <p className='mb-1 text-secondary'>
-                        {Role} @ {company}
-                      </p>
-                    </div>
+    <div className='main-content-card'>
+      <div
+        className='container'
+        style={{
+          width: "100%",
+        }}
+      >
+        <div className='row'>
+          <div className='col-md-12'>
+            <div className='card mb-3'>
+              <div
+                className='card-body'
+                style={{
+                  padding: "1.5rem 4rem",
+                }}
+              >
+                <div className='d-flex flex-column align-items-center text-center'>
+                  <ContactAvatar
+                    name={contact.fname}
+                    className={"info-avatar"}
+                  />
+                  <div className='mt-3'>
+                    <h4>{contact.fname}</h4>
+                    <p className='mb-1 text-secondary'>
+                      {contact.Role} @ {contact.company}
+                    </p>
                   </div>
-                  <div className='row mt-3'>
-                    <div className='col-sm-3'>
-                      <h6 className=' mb-0 text-secondary'>Full Name</h6>
-                    </div>
-                    <div className='col-sm-9  '>{fname}</div>
+                </div>
+                <div className='row mt-3'>
+                  <div className='col-sm-4'>
+                    <h6 className=' mb-0 text-secondary'>Full Name</h6>
                   </div>
-                  <hr />
-                  <div className='row'>
-                    <div className='col-sm-3'>
-                      <h6 className=' mb-0 text-secondary '>Email</h6>
-                    </div>
-                    <div className='col-sm-9'>{email}</div>
+                  <div className='col-sm-8  '>{contact.fname}</div>
+                </div>
+                <hr />
+                <div className='row'>
+                  <div className='col-sm-4'>
+                    <h6 className=' mb-0 text-secondary '>Email</h6>
                   </div>
-                  <hr />
-                  <div className='row'>
-                    <div className='col-sm-3'>
-                      <h6 className=' mb-0 text-secondary'>Phone</h6>
-                    </div>
-                    <div className='col-sm-9  '>{phone}</div>
+                  <div className='col-sm-8'>{contact.email}</div>
+                </div>
+                <hr />
+                <div className='row'>
+                  <div className='col-sm-4'>
+                    <h6 className=' mb-0 text-secondary'>Phone</h6>
                   </div>
-                  <hr />
-                  <div className='row'>
-                    <div className='col-sm-3'>
-                      <h6 className=' mb-0 text-secondary'>Company</h6>
-                    </div>
-                    <div className='col-sm-9  '>{company}</div>
+                  <div className='col-sm-8  '>{contact.phone}</div>
+                </div>
+                <hr />
+                <div className='row'>
+                  <div className='col-sm-4'>
+                    <h6 className=' mb-0 text-secondary'>Company</h6>
                   </div>
-                  <hr />
-                  <div className='row'>
-                    <div className='col-sm-3'>
-                      <h6 className=' mb-0 text-secondary'>Address</h6>
-                    </div>
-                    <div className='col-sm-9  '>{address}</div>
+                  <div className='col-sm-8  '>{contact.company}</div>
+                </div>
+                <hr />
+                <div className='row'>
+                  <div className='col-sm-4'>
+                    <h6 className=' mb-0 text-secondary'>Address</h6>
                   </div>
-                  <hr />
-                  <div className='row'>
-                    <div className='col-sm-12 text-center'>
-                      <EditContact />
-                    </div>
+                  <div className='col-sm-8  '>{contact.address}</div>
+                </div>
+                <hr />
+                <div className='row'>
+                  <div className='col-sm-12 text-center'>
+                    {/* <EditContact /> */}
+                    <ContactButton
+                      btnIcon={"pencil"}
+                      btnText={"Edit Contact"}
+                      setModalShow={props.setModalShow}
+                      setIsEdit={props.setIsEdit}
+                    />
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className='row'></div>
-            <div className='row'></div>
           </div>
+
+          <div className='row'></div>
+          <div className='row'></div>
         </div>
       </div>
     </div>
