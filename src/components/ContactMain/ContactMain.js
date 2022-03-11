551f7a84711a1e9b1be52ac82eee0a567aea366f
Vishal Rathod <rvd31305@gmail.com>
2022-03-11 18:12:35 +0530

    addToLocalStorage, model, active user info, contact button
    - addToLocalStorageFunction replaced with useEffect
    - model moved from button to main component
    - Only active user info visible initially nothing rendered
    - Contact button added

--- src/components/ContactMain/ContactMain.js
+++ src/components/ContactMain/ContactMain.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import SearchBar from "../SearchBar/SearchBar";
-import AddContact from "../AddContact/AddContact";
 import ContactTable from "../ContactTable/ContactTable";
 import ContactInfo from "../ContactInfo/ContactInfo";
+import ContactButton from "../ContactButton/ContactButton";
+import ContactModel from "../ContactModel/ContactModel";
 
 import Logo from "../../contact-list.png";
 
@@ -26,24 +27,27 @@ const ContactMain = () => {
         ]
   );
 
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   const [filterText, setFilterText] = useState("");
 
-  const [activeContact, setActiveContact] = useState(contacts[0]);
+  const [activeContact, setActiveContact] = useState(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const [modalShow, setModalShow] = useState(false);
+
+  const [isEdit, setIsEdit] = useState(false);
 
   const addContact = (contact) => {
-    const newContacts = [...contacts, contact];
-    setContacts(newContacts);
-    addToLocalStorage(newContacts);
-  };
-
-  const addToLocalStorage = (contacts) => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
+    setContacts([...contacts, contact]);
   };
 
   const deleteContact = (id) => {
     const newContacts = contacts.filter((contact) => contact.id !== id);
     setContacts(newContacts);
-    addToLocalStorage(newContacts);
+    setIsActive(false);
   };
 
   const editContact = (contact) => {
@@ -51,12 +55,13 @@ const ContactMain = () => {
       c.id === contact.id ? contact : c
     );
     setContacts(newContacts);
-    addToLocalStorage(newContacts);
+    setActiveContact(contact);
   };
 
   const showActiveUser = (id) => {
     const newActiveContact = contacts.find((contact) => contact.id === id);
     setActiveContact(newActiveContact);
+    setIsActive(true);
   };
 
   return (
@@ -81,24 +86,49 @@ const ContactMain = () => {
             </div>
           </div>
           <div className='row mt-4'>
-            <div className='col-md-2 d-flex'>
+            <div className='col-md-3 d-flex'>
               <SearchBar
                 filterText={filterText}
                 setFilterText={setFilterText}
               />
             </div>
-            <div className='col-md-10 d-flex align-center'>
-              <AddContact addContact={addContact} />
+            <div className='col-md-9 d-flex align-center'>
+              <ContactButton
+                btnIcon={"plus"}
+                btnText={"Add Contact"}
+                btnType={"add"}
+                setModalShow={setModalShow}
+                setIsEdit={setIsEdit}
+              />
             </div>
           </div>
           <div className='row mt-5'>
-            <ContactTable
-              contacts={contacts}
-              deleteContact={deleteContact}
-              filterText={filterText}
-              showActiveUser={showActiveUser}
+            <div className='col-md-7 '>
+              <ContactTable
+                contacts={contacts}
+                deleteContact={deleteContact}
+                filterText={filterText}
+                showActiveUser={showActiveUser}
+              />
+            </div>
+            <div className='col-md-5'>
+              {isActive && (
+                <ContactInfo
+                  activeContact={activeContact}
+                  setIsEdit={setIsEdit}
+                  setModalShow={setModalShow}
+                />
+              )}
+            </div>
+            <ContactModel
+              activeContact={activeContact}
+              isEdit={isEdit}
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+              title={isEdit ? "Edit Contact" : "Add Contact"}
+              addContact={addContact}
+              editContact={editContact}
             />
-            <ContactInfo activeContact={activeContact} />
           </div>
         </div>
       </div>
