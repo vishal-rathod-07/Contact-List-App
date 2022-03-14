900b70644254268f0c3daa3a63806108cabcc773
Vishal Rathod <rvd31305@gmail.com>
2022-03-14 16:21:52 +0530

    Refactor: classes refactored for responsiveness

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -35,7 +35,7 @@ const ContactInfo = (props) => {
   return (
     <div className='main-content-card'>
       <div
-        className='container'
+        className='container p-0'
         style={{
           width: '100%',
         }}
@@ -43,12 +43,7 @@ const ContactInfo = (props) => {
         <div className='row'>
           <div className='col-md-12'>
             <div className='card mb-3'>
-              <div
-                className='card-body'
-                style={{
-                  padding: '1.5rem 4rem',
-                }}
-              >
+              <div className='card-body'>
                 <div className='d-flex flex-column align-items-center text-center'>
                   <ContactAvatar
                     name={contact.fname + ' ' + contact.lname}
@@ -62,44 +57,44 @@ const ContactInfo = (props) => {
                   </div>
                 </div>
                 <div className='row mt-3'>
-                  <div className='col-sm-4'>
+                  <div className='col-4'>
                     <h6 className=' mb-0 text-secondary'>Full Name</h6>
                   </div>
-                  <div className='col-sm-8  '>
+                  <div className='col-8  '>
                     {contact.fname} {contact.lname}
                   </div>
                 </div>
                 <hr />
                 <div className='row'>
-                  <div className='col-sm-4'>
+                  <div className='col-4'>
                     <h6 className=' mb-0 text-secondary '>Email</h6>
                   </div>
-                  <div className='col-sm-8'>{contact.email}</div>
+                  <div className='col-8'>{contact.email}</div>
                 </div>
                 <hr />
                 <div className='row'>
-                  <div className='col-sm-4'>
+                  <div className='col-4'>
                     <h6 className=' mb-0 text-secondary'>Phone</h6>
                   </div>
-                  <div className='col-sm-8  '>{contact.phone}</div>
+                  <div className='col-8  '>{contact.phone}</div>
                 </div>
                 <hr />
                 <div className='row'>
-                  <div className='col-sm-4'>
+                  <div className='col-4'>
                     <h6 className=' mb-0 text-secondary'>Company</h6>
                   </div>
-                  <div className='col-sm-8  '>{contact.company}</div>
+                  <div className='col-8  '>{contact.company}</div>
                 </div>
                 <hr />
                 <div className='row'>
-                  <div className='col-sm-4'>
+                  <div className='col-4'>
                     <h6 className=' mb-0 text-secondary'>Address</h6>
                   </div>
-                  <div className='col-sm-8  '>{contact.address}</div>
+                  <div className='col-8  '>{contact.address}</div>
                 </div>
                 <hr />
                 <div className='row'>
-                  <div className='col-sm-12 text-center'>
+                  <div className='col-12 text-center'>
                     {/* <EditContact /> */}
                     <ContactButton
                       btnIcon={'pencil'}
