fda7ea93f241a059e6000252636ce83810fb4948
Vishal Rathod <rvd31305@gmail.com>
2022-03-14 16:25:43 +0530

    Wrapper added to sidebar for display none in small devices

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -11,47 +11,49 @@ import './sidebar.scss';
 
 const SideBar = () => {
   return (
-    <div className='sidebar d-flex flex-column flex-shrink-0 bg-light'>
-      <div className='sidebar__header-logo'>
-        <MenuAlt2Icon className=' sidebar__icon' />
+    <div className='sidebar_wrapper d-none d-md-block'>
+      <div className='sidebar d-flex flex-column flex-shrink-0 bg-light'>
+        <div className='sidebar__header-logo'>
+          <MenuAlt2Icon className=' sidebar__icon' />
+        </div>
+        <ul className='nav nav-pills nav-flush flex-column mb-auto text-center mt-5'>
+          <li className='nav-item'>
+            <a href='#' className='nav-link py-3  '>
+              <HomeIcon className=' sidebar__icon' />
+            </a>
+          </li>
+          <li>
+            <a href='#' className='nav-link active py-3  '>
+              <UserIcon className='sidebar__icon' />
+            </a>
+          </li>
+          <li>
+            <a href='#' className='nav-link py-3  '>
+              <ClipboardListIcon className=' sidebar__icon' />
+            </a>
+          </li>
+          <li>
+            <a href='#' className='nav-link py-3  '>
+              <ChartPieIcon className=' sidebar__icon' />
+            </a>
+          </li>
+          <li>
+            <a href='#' className='nav-link py-3  '>
+              <DatabaseIcon className=' sidebar__icon' />
+            </a>
+          </li>
+          <li>
+            <a href='#' className='nav-link py-3  '>
+              <CalendarIcon className=' sidebar__icon' />
+            </a>
+          </li>
+          <li>
+            <a href='#' className='nav-link py-3  '>
+              <CogIcon className=' sidebar__icon' />
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul className='nav nav-pills nav-flush flex-column mb-auto text-center mt-5'>
-        <li className='nav-item'>
-          <a href='#' className='nav-link py-3  '>
-            <HomeIcon className=' sidebar__icon' />
-          </a>
-        </li>
-        <li>
-          <a href='#' className='nav-link active py-3  '>
-            <UserIcon className='sidebar__icon' />
-          </a>
-        </li>
-        <li>
-          <a href='#' className='nav-link py-3  '>
-            <ClipboardListIcon className=' sidebar__icon' />
-          </a>
-        </li>
-        <li>
-          <a href='#' className='nav-link py-3  '>
-            <ChartPieIcon className=' sidebar__icon' />
-          </a>
-        </li>
-        <li>
-          <a href='#' className='nav-link py-3  '>
-            <DatabaseIcon className=' sidebar__icon' />
-          </a>
-        </li>
-        <li>
-          <a href='#' className='nav-link py-3  '>
-            <CalendarIcon className=' sidebar__icon' />
-          </a>
-        </li>
-        <li>
-          <a href='#' className='nav-link py-3  '>
-            <CogIcon className=' sidebar__icon' />
-          </a>
-        </li>
-      </ul>
     </div>
   );
 };
