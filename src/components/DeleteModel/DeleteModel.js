89794a1a5de70ae1a2407af37ff42284bbfbf72d
Vishal Rathod <rvd31305@gmail.com>
2022-03-15 18:29:02 +0530

    Confirm delete model added

---
+++ src/components/DeleteModel/DeleteModel.js
@@ -0,0 +1,33 @@
+import { Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+
+const DeleteModel = ({ show, handleClose, handleDelete }) => {
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size='md'
+        aria-labelledby='contained-modal-title-vcenter'
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete this contact(s)?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant='danger' onClick={handleDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default DeleteModel;
