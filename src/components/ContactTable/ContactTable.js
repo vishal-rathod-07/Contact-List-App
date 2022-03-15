d11a1e020d8c14ac953821a8a0abdccff75bd14e
Vishal Rathod <rvd31305@gmail.com>
2022-03-15 11:42:36 +0530

    Multi delete functionality, No contact row added
    - Multiple delete functionality added
    - If there are no contacts in data base then no contacts found message row displayed

--- src/components/ContactTable/ContactTable.js
+++ src/components/ContactTable/ContactTable.js
@@ -1,3 +1,4 @@
+import { TrashIcon } from '@heroicons/react/outline';
 import { Table } from 'react-bootstrap';
 import ContactRow from '../ContactRow/ContactRow';
 
@@ -8,6 +9,9 @@ const ContactTable = ({
   filterText,
   deleteContact,
   showActiveUser,
+  deleteCheckedContacts,
+  checkedContactIdList,
+  setCheckedContactIDList,
 }) => {
   return (
     <div className='main-content-list'>
@@ -17,43 +21,62 @@ const ContactTable = ({
             <th className='text-center'>+</th>
             <th className='text-secondary'>Basic Info</th>
             <th className='text-secondary'>Company</th>
-            <th className='text-center'></th>
+            <th className='text-center p-1'>
+              {checkedContactIdList.length > 0 ? (
+                <TrashIcon
+                  className='trash-icon'
+                  onClick={() => deleteCheckedContacts(checkedContactIdList)}
+                />
+              ) : (
+                ''
+              )}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {contacts
-            .filter(
-              (contact) =>
-                contact.fname
-                  .toLowerCase()
-                  .includes(filterText.toLowerCase()) ||
-                contact.lname
-                  .toLowerCase()
-                  .includes(filterText.toLowerCase()) ||
-                (
-                  contact.fname.toLowerCase() +
-                  ' ' +
-                  contact.lname.toLowerCase()
-                ).includes(filterText.toLowerCase()) ||
-                contact.company
-                  .toLowerCase()
-                  .includes(filterText.toLowerCase()) ||
-                contact.email
-                  .toLowerCase()
-                  .includes(filterText.toLowerCase()) ||
-                contact.phone
-                  .toLowerCase()
-                  .includes(filterText.toLowerCase()) ||
-                contact.Role.toLowerCase().includes(filterText.toLowerCase())
-            )
-            .map((contact) => (
-              <ContactRow
-                key={contact.id}
-                contact={contact}
-                deleteContact={deleteContact}
-                showActiveUser={showActiveUser}
-              />
-            ))}
+          {contacts.length > 0 ? (
+            contacts
+              .filter(
+                (contact) =>
+                  contact.fname
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+                  contact.lname
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+                  (
+                    contact.fname.toLowerCase() +
+                    ' ' +
+                    contact.lname.toLowerCase()
+                  ).includes(filterText.toLowerCase()) ||
+                  contact.company
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+                  contact.email
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+                  contact.phone
+                    .toLowerCase()
+                    .includes(filterText.toLowerCase()) ||
+                  contact.Role.toLowerCase().includes(filterText.toLowerCase())
+              )
+              .map((contact) => (
+                <ContactRow
+                  key={contact.id}
+                  contact={contact}
+                  deleteContact={deleteContact}
+                  showActiveUser={showActiveUser}
+                  checkedContactIdList={checkedContactIdList}
+                  setCheckedContactIDList={setCheckedContactIDList}
+                />
+              ))
+          ) : (
+            <tr>
+              <td colSpan='4' className='text-center text-secondary fw-bold'>
+                No contacts found
+              </td>
+            </tr>
+          )}
         </tbody>
       </Table>
     </div>
