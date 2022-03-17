3b5ad6463be8ea2d6bfb656fb6f56986b0741b0a
Vishal Rathod <rvd31305@gmail.com>
2022-03-17 12:33:02 +0530

    Random colour generating logic changed

--- src/components/ContactAvatar/ContactAvatar.js
+++ src/components/ContactAvatar/ContactAvatar.js
@@ -1,24 +1,9 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
+import md5 from 'md5';
 
 function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.substr(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
+  return '#' + md5(string).slice(0, 6);
 }
 
 function stringAvatar(name) {
