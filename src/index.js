17ff7e75b66e3b90b564fcb051c95cd12e3dca7a
Vishal Rathod <rvd31305@gmail.com>
2022-03-19 11:54:00 +0530

    Imports rearranged

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 ReactDOM.render(
